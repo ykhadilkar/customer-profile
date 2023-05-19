2ed02187c1200c85c71fd8636d57c25e10a33052
Yatin Khadilkar <ykhadilkar@vmware.com>
2023-05-19 15:43:05 -0400

    upgrading to boot 3

--- src/main/java/com/example/customerprofile/domain/CustomerProfileResponse.java
+++ src/main/java/com/example/customerprofile/domain/CustomerProfileResponse.java
@@ -1,6 +1,6 @@
 package com.example.customerprofile.domain;
 
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 
 public class CustomerProfileResponse {
 
